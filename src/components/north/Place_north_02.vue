bc92cb1b861eeb9f499abc19e924bf68d46e9164
Eshull2p1 <141694947+Eshull2p1@users.noreply.github.com>
2023-10-14 15:28:39 +0700

    มาเหนือเรสปอน

--- src/components/north/Place_north_02.vue
+++ src/components/north/Place_north_02.vue
@@ -8,10 +8,23 @@ const item_advice_north_02 = item_advice_north_02Store()
 const item_advice_north_02_all = ref(item_advice_north_02.item_advice_north_list_02)
 </script>
 <template>
-  <!-- mainbox -->
   <!-- แถบ ภาพ  -->
   <div class="imgTitle">
-    <div class="textTitle">
+    <router-link :to="{ name: 'main' }">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="currentColor"
+        class="bi bi-house house-icon"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"
+        />
+      </svg>
+    </router-link>
+    <div>
       <h2>สวนสนแม่แตง</h2>
       <p>สถานที่ที่ร่มรื่นเหมาะกับวันพักผ่อน</p>
     </div>
@@ -20,36 +33,36 @@ const item_advice_north_02_all = ref(item_advice_north_02.item_advice_north_list
   <Header_box_north></Header_box_north>
   <!-- แถบ เนื้อหา -->
   <!-- box รูปซ้าย -->
-  <div class="container">
-    <div class="LPage">
+  <div class="container-n2">
+    <div class="LPage-n2">
       <img
         src="https://www.paiduaykan.com/travel/wp-content/uploads/2021/12/1-SON00554.jpg"
         class="imgBow"
       />
-      <div class="container_text">
+      <div style="margin-block: 10px;">
         <h5 class="card-title">สวนสนแม่แตง</h5>
-        <p class="textConten">
+        <p class="textConten-n2">
           สวนสนแม่แตง หรือสวนผลิตเมล็ดพันธ์สองใบ
           ตั้งอยู่ในเขตพื้นที่รับผิดชอบของสถานีวนวัฒนาวิจัยอินทรขิล จังหวัดเชียงใหม่
           ซึ่งเป็นสถานีงานวิจัยด้านป่าไม้ บรรยากาศรอบๆ ของสวนสนแห่งนี้ดูร่มรื่นสบายตา
           จึงเหมาะแก่การมาถ่ายภาพ หรือจะมาพักผ่อนหย่อนใจก็ได้ค่ะ
           ภายในจะมีจุดถ่ายภาพให้กับนักท่องเที่ยว บริเวณกว้างขว้างมากๆ เลือกมุมถ่ายภาพได้ตามใจกันเลย
         </p>
-        <a href="https://travel.trueid.net/detail/kW22p6w5JexW" class="btn_cart">อ่านเพิ่มเติม</a>
+        <a href="https://travel.trueid.net/detail/kW22p6w5JexW" class="btn_cart-n2"
+          >อ่านเพิ่มเติม</a
+        >
       </div>
     </div>
     <!--box รูปขวา-->
-    <div class="RPage">
-      <div class="container_text">
-        <p class="textConten">
-          สวนผลิตเมล็ดพันธ์สนสองใบหรือ สวนสนแม่แตง ตั้งอยู่ติดถนนหมายเลข 107
-          เส้นทางแม่แตงมุ่งหน้าสู่อำเภอเชียงดาวและฝา
-          เป็นเส้นทางผ่านไปยังสองอำเภอนี้สามารถแวะเที่ยวก่อนไปต่อยังจุดอื่นได้
-          ลักษณะเป็นสวนสนคาริเบียน ในเนื้อที่ 27 ไร่
-          ซึ่งเป็นงานวิจัยการพัฒนาและฟื้นฟูทดลองปลูกสนคาริเบีย
-          ภายในสวนสวยอากาศร่มรื่นเย็นสบายตลอดทั้งวัน
-        </p>
-      </div>
+    <div class="RPage-n2">
+      <p class="textConten-n2">
+        สวนผลิตเมล็ดพันธ์สนสองใบหรือ สวนสนแม่แตง ตั้งอยู่ติดถนนหมายเลข 107
+        เส้นทางแม่แตงมุ่งหน้าสู่อำเภอเชียงดาวและฝา
+        เป็นเส้นทางผ่านไปยังสองอำเภอนี้สามารถแวะเที่ยวก่อนไปต่อยังจุดอื่นได้
+        ลักษณะเป็นสวนสนคาริเบียน ในเนื้อที่ 27 ไร่
+        ซึ่งเป็นงานวิจัยการพัฒนาและฟื้นฟูทดลองปลูกสนคาริเบีย
+        ภายในสวนสวยอากาศร่มรื่นเย็นสบายตลอดทั้งวัน
+      </p>
       <img
         src="https://img.wongnai.com/p/1920x0/2018/09/11/87d33f9b8d34413ba023c77b1f6c3691.jpg"
         class="imgBow"
@@ -57,14 +70,14 @@ const item_advice_north_02_all = ref(item_advice_north_02.item_advice_north_list
     </div>
     <!-- สิ่งของแนะนำขาย -->
     <h1 style="text-align: center">สิ่งที่ควรเตรียมไป</h1>
-    <div class="recomen">
-      <div class="re_conten" v-for="(i, index) in item_advice_north_02_all" :key="index">
+    <div class="recomen-n2">
+      <div class="re_conten-n2" v-for="(i, index) in item_advice_north_02_all" :key="index">
         <div>
           <div class="card">
             <img :src="i.img" />
           </div>
         </div>
-        <div class="re_text">
+        <div class="re_text-n2">
           <h4>{{ i.name }}</h4>
           <p>{{ i.category }}</p>
         </div>
@@ -73,82 +86,248 @@ const item_advice_north_02_all = ref(item_advice_north_02.item_advice_north_list
   </div>
 </template>
 <style scoped>
-.container {
-  margin-block: 20px;
+@media (min-width: 1030px) {
+  .container-n2 {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 10px;
+    justify-items: center;
+    align-items: center;
+  }
+  .imgTitle {
+    max-width: 100%;
+    background-position: left bottom;
+    background-size: cover;
+    padding-top: 200px;
+    padding-bottom: 100px;
+    background-image: url(https://i.pinimg.com/564x/a4/11/bf/a411bfbf71e49fc1a525fccf0bbc0d40.jpg);
+  }
+  .btn_cart-n2 {
+    padding: 5px;
+    color: white;
+    text-decoration: none;
+    background-color: black;
+    border-radius: 5px;
+  }
+  .btn_cart-n2:hover {
+    padding: 8px;
+    color: white;
+    text-decoration: none;
+    background-color: rgb(4, 60, 203);
+    border-radius: 5px;
+  }
+  .LPage-n2 {
+    display: grid;
+    grid-template-columns: 400px auto;
+    column-gap: 10px;
+    justify-items: stretch;
+    align-items: center;
+    margin-inline: 170px;
+    margin-block: 5px;
+    width: 1000px;
+    border-bottom: 2px solid;
+  }
+  .RPage-n2 {
+    display: grid;
+    grid-template-columns: auto 400px;
+    column-gap: 1px;
+    justify-items: stretch;
+    align-items: center;
+    margin-inline: 170px;
+    margin-block: 5px;
+    width: 1000px;
+    border-bottom: 2px solid;
+  }
+  .imgBow {
+    max-width: 880px;
+    margin: 10px;
+  }
+  .container_text {
+    padding: 5px;
+  }
+  .textConten {
+    padding: 20px;
+    width: 500px;
+  }
+  .recomen-n2 {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: stretch;
+    align-items: stretch;
+    row-gap: 2px;
+    column-gap: 2px;
+    max-width: 1000px;
+    margin-inline: 200px;
+    margin-block: 10px;
+  }
+  .re_conten-n2 {
+    display: grid;
+    grid-template-columns: 200px auto;
+    column-gap: 10px;
+    justify-items: stretch;
+    align-items: center;
+    box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1);
+    padding: 5px;
+  }
 }
-.imgTitle {
-  max-width: 100%;
-  background-position: left bottom;
-  background-size: cover;
-  padding-top: 200px;
-  padding-bottom: 100px;
-  background-image: url(https://www.paiduaykan.com/travel/wp-content/uploads/2021/12/2-SON00547.jpg);
+@media (min-width: 768px) and (max-width: 1024px) {
+  .container-n2 {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 10px;
+    justify-items: center;
+    align-items: stretch;
+  }
+  .imgTitle {
+    max-width: 100%;
+    background-position: left bottom;
+    background-size: cover;
+    padding-top: 200px;
+    padding-bottom: 100px;
+    background-image: url(https://i.pinimg.com/564x/a4/11/bf/a411bfbf71e49fc1a525fccf0bbc0d40.jpg);
+  }
+  .btn_cart-n2 {
+    padding: 5px;
+    color: white;
+    text-decoration: none;
+    background-color: black;
+    border-radius: 5px;
+  }
+  .btn_cart-n2:hover {
+    padding: 8px;
+    color: white;
+    text-decoration: none;
+    background-color: rgb(4, 60, 203);
+    border-radius: 5px;
+  }
+  .LPage-n2 {
+    display: grid;
+    grid-template-columns: 300px auto;
+    column-gap: 10px;
+    justify-items: stretch;
+    align-items: center;
+    max-width: 800px;
+    border-bottom: 2px solid;
+  }
+  .RPage-n2 {
+    display: grid;
+    grid-template-columns: auto 300px;
+    column-gap: 1px;
+    justify-items: stretch;
+    align-items: center;
+    max-width: 800px;
+    border-bottom: 2px solid;
+  }
+  .imgBow {
+    max-width: 880px;
+    margin: 10px;
+  }
+  .textConten-n2 {
+    padding: 20px;
+    height: 100px;
+    width: 400px;
+    overflow: auto;
+  }
+  .recomen-n2 {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: stretch;
+    align-items: stretch;
+    row-gap: 2px;
+    column-gap: 2px;
+    margin-block: 10px;
+    max-width: 900px;
+    max-height: 300px;
+  }
+  .re_conten-n2 {
+    display: grid;
+    grid-template-columns: 100px auto;
+    column-gap: 10px;
+    justify-items: stretch;
+    align-items: center;
+    box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1);
+    padding: 5px;
+    box-sizing: content-box;
+  }
 }
-.btn_cart {
-  padding: 5px;
-  color: white;
-  text-decoration: none;
-  background-color: black;
-  border-radius: 5px;
-}
-.btn_cart:hover {
-  padding: 8px;
-  color: white;
-  text-decoration: none;
-  background-color: rgb(4, 60, 203);
-  border-radius: 5px;
-}
-.LPage {
-  display: grid;
-  grid-template-columns: 400px auto;
-  column-gap: 10px;
-  justify-items: stretch;
-  align-items: center;
-  margin-inline: 170px;
-  margin-block: 5px;
-  width: 1000px;
-  border-bottom: 2px solid;
-}
-.RPage {
-  display: grid;
-  grid-template-columns: auto 400px;
-  column-gap: 1px;
-  justify-items: stretch;
-  align-items: center;
-  margin-inline: 170px;
-  margin-block: 5px;
-  width: 1000px;
-  border-bottom: 2px solid;
-}
-.imgBow {
-  max-width: 880px;
-  margin: 10px;
-}
-.container_text {
-  padding: 5px;
-}
-.textConten {
-  padding: 20px;
-  width: 500px;
-}
-.recomen {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  justify-items: stretch;
-  align-items: stretch;
-  row-gap: 2px;
-  column-gap: 2px;
-  max-width: 1000px;
-  margin-inline: 200px;
-  margin-block: 10px;
-}
-.re_conten {
-  display: grid;
-  grid-template-columns: 200px auto;
-  column-gap: 10px;
-  justify-items: stretch;
-  align-items: center;
-  box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1);
-  padding: 5px;
+@media (max-width: 767px) {
+  .container-n2 {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 10px;
+    justify-items: center;
+    align-items: stretch;
+  }
+  .imgTitle {
+    max-width: 100%;
+    background-position: left bottom;
+    background-size: cover;
+    padding-top: 200px;
+    padding-bottom: 100px;
+    background-image: url(https://i.pinimg.com/564x/a4/11/bf/a411bfbf71e49fc1a525fccf0bbc0d40.jpg);
+  }
+  .btn_cart-n2 {
+    padding: 5px;
+    color: white;
+    text-decoration: none;
+    background-color: black;
+    border-radius: 5px;
+  }
+  .btn_cart-n2:hover {
+    padding: 8px;
+    color: white;
+    text-decoration: none;
+    background-color: rgb(4, 60, 203);
+    border-radius: 5px;
+  }
+  .LPage-n2 {
+    display: grid;
+    grid-template-columns: 1fr;
+    justify-items: stretch;
+    border-bottom: 2px solid;
+    margin-block: 5px;
+    max-width: 300px;
+  }
+  .RPage-n2 {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap-reverse;
+    justify-content: center;
+    align-items: stretch;
+    justify-items: stretch;
+    max-width: 300px;
+    margin-block: 5px;
+    border-bottom: 2px solid;
+  }
+  .imgBow {
+    max-width: 880px;
+    margin: 10px;
+  }
+  .textConten-n2 {
+    padding: 20px;
+    height: 150px;
+    width: 300px;
+    overflow: auto;
+  }
+  .recomen-n2 {
+    display: grid;
+    grid-template-columns: 1fr;
+    justify-items: stretch;
+    align-items: stretch;
+    row-gap: 2px;
+    column-gap: 2px;
+    margin-block: 10px;
+    max-width: 600px;
+  }
+  .re_conten-n2 {
+    display: grid;
+    grid-template-columns: 100px auto;
+    column-gap: 10px;
+    justify-items: stretch;
+    align-items: center;
+    box-sizing: content-box;
+    box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1);
+    padding: 5px;
+  }
 }
 </style>
--- src/components/north/Place_north_05.vue
+++ src/components/north/Place_north_05.vue
@@ -10,7 +10,21 @@ const item_advice_north_05_all = ref(item_advice_north_05.item_advice_north_list
 <template>
   <!-- แถบ ภาพ  -->
   <div class="imgTitle">
-    <div class="textTitle">
+    <router-link :to="{ name: 'main' }">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="currentColor"
+        class="bi bi-house house-icon"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"
+        />
+      </svg>
+    </router-link>
+    <div>
       <h2>เขื่อนแม่งัด อุทยานแห่งชาติศรีลานนา</h2>
       <p>อุทยานแห่งชาติที่ควรมาเยือนซักครั้ง</p>
     </div>
@@ -20,25 +34,25 @@ const item_advice_north_05_all = ref(item_advice_north_05.item_advice_north_list
 
   <!-- แถบ เนื้อหา -->
   <!-- box รูปซ้าย -->
-  <div class="container">
-    <div class="LPage">
+  <div class="container-n5">
+    <div class="LPage-n5">
       <img
         src="https://i.pinimg.com/564x/e5/98/6e/e5986ead821600e86c01eac7079d1bf6.jpg"
         class="imgBow"
       />
-      <div class="container_text">
+      <div style="margin-block: 10px;">
         <h2 class="card-title">เขื่อนแม่งัด อุทยานแห่งชาติศรีลานนา</h2>
-        <p class="textConten">
+        <p class="textConten-n5">
           เขื่อนแม่งัด ตั้งอยู่ในเขตอุทยานแห่งชาติศรีลานนา จังหวัดเชียงใหม่ และอยู่ไม่ไกลจาก
           วัดบ้านเด่น มากนัก ซึ่งวิวทิวทัศน์ของเขื่อนล้อมรอบไปด้วยภูเขาที่เขียวขจี
         </p>
-        <a href="https://travel.trueid.net/detail/kW22p6w5JexW" class="btn_cart">อ้างอิงมาจาก</a>
+        <a href="https://travel.trueid.net/detail/kW22p6w5JexW" class="btn_cart-n5">อ้างอิงมาจาก</a>
       </div>
     </div>
     <!--box รูปขวา-->
-    <div class="RPage">
-      <div class="container_text">
-        <p class="textConten">
+    <div class="RPage-n5">
+      <div class="container_text-n5">
+        <p class="textConten-n5">
           ความสำคัญของเขื่อนแม่งัด คือ
           การทำหน้าที่ส่งน้ำหล่อเลี้ยงพื้นที่เพาะปลูกในที่ราบเชิงเขาทั้งสองฝั่งของลำน้ำแม่งัด
           อีกทั้งยังช่วยบรรเทาอุทกภัยในเขตลุ่มน้ำปิง โดยเฉพาะบรรเทาน้ำหลากเข้าท่วมตัวเมืองเชียงใหม่
@@ -56,8 +70,8 @@ const item_advice_north_05_all = ref(item_advice_north_05.item_advice_north_list
 
     <!-- สิ่งของแนะนำขาย -->
     <h1 style="text-align: center">สิ่งที่ควรเตรียมไป</h1>
-    <div class="recomen">
-      <div class="re_conten" v-for="(i, index) in item_advice_north_05_all" :key="index">
+    <div class="recomen-n5">
+      <div class="re_conten-n5" v-for="(i, index) in item_advice_north_05_all" :key="index">
         <div>
           <div class="card">
             <img :src="i.img" />
@@ -72,84 +86,248 @@ const item_advice_north_05_all = ref(item_advice_north_05.item_advice_north_list
   </div>
 </template>
 <style scoped>
-.container {
-  margin-block: 20px;
+@media (min-width: 1030px) {
+  .container-n5 {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 10px;
+    justify-items: center;
+    align-items: center;
+  }
+  .imgTitle {
+    max-width: 100%;
+    background-position: left bottom;
+    background-size: cover;
+    padding-top: 200px;
+    padding-bottom: 100px;
+    background-image: url(https://i.pinimg.com/564x/a4/11/bf/a411bfbf71e49fc1a525fccf0bbc0d40.jpg);
+  }
+  .btn_cart-n5 {
+    padding: 5px;
+    color: white;
+    text-decoration: none;
+    background-color: black;
+    border-radius: 5px;
+  }
+  .btn_cart-n5:hover {
+    padding: 8px;
+    color: white;
+    text-decoration: none;
+    background-color: rgb(4, 60, 203);
+    border-radius: 5px;
+  }
+  .LPage-n5 {
+    display: grid;
+    grid-template-columns: 400px auto;
+    column-gap: 10px;
+    justify-items: stretch;
+    align-items: center;
+    margin-inline: 170px;
+    margin-block: 5px;
+    width: 1000px;
+    border-bottom: 2px solid;
+  }
+  .RPage-n5 {
+    display: grid;
+    grid-template-columns: auto 400px;
+    column-gap: 1px;
+    justify-items: stretch;
+    align-items: center;
+    margin-inline: 170px;
+    margin-block: 5px;
+    width: 1000px;
+    border-bottom: 2px solid;
+  }
+  .imgBow {
+    max-width: 880px;
+    margin: 10px;
+  }
+  .container_text {
+    padding: 5px;
+  }
+  .textConten-n5 {
+    padding: 20px;
+    width: 500px;
+  }
+  .recomen-n5 {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: stretch;
+    align-items: stretch;
+    row-gap: 2px;
+    column-gap: 2px;
+    max-width: 1000px;
+    margin-inline: 200px;
+    margin-block: 10px;
+  }
+  .re_conten-n5 {
+    display: grid;
+    grid-template-columns: 200px auto;
+    column-gap: 10px;
+    justify-items: stretch;
+    align-items: center;
+    box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1);
+    padding: 5px;
+  }
 }
-.imgTitle {
-  max-width: 100%;
-  background-position: left bottom;
-  background-size: cover;
-  padding-top: 200px;
-  padding-bottom: 100px;
-  background-image: url(https://i.pinimg.com/564x/e5/98/6e/e5986ead821600e86c01eac7079d1bf6.jpg);
+@media (min-width: 768px) and (max-width: 1024px) {
+  .container-n5 {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 10px;
+    justify-items: center;
+    align-items: stretch;
+  }
+  .imgTitle {
+    max-width: 100%;
+    background-position: left bottom;
+    background-size: cover;
+    padding-top: 200px;
+    padding-bottom: 100px;
+    background-image: url(https://i.pinimg.com/564x/a4/11/bf/a411bfbf71e49fc1a525fccf0bbc0d40.jpg);
+  }
+  .btn_cart-n5 {
+    padding: 5px;
+    color: white;
+    text-decoration: none;
+    background-color: black;
+    border-radius: 5px;
+  }
+  .btn_cart-n5:hover {
+    padding: 8px;
+    color: white;
+    text-decoration: none;
+    background-color: rgb(4, 60, 203);
+    border-radius: 5px;
+  }
+  .LPage-n5 {
+    display: grid;
+    grid-template-columns: 300px auto;
+    column-gap: 10px;
+    justify-items: stretch;
+    align-items: center;
+    max-width: 800px;
+    border-bottom: 2px solid;
+  }
+  .RPage-n5 {
+    display: grid;
+    grid-template-columns: auto 300px;
+    column-gap: 1px;
+    justify-items: stretch;
+    align-items: center;
+    max-width: 800px;
+    border-bottom: 2px solid;
+  }
+  .imgBow {
+    max-width: 880px;
+    margin: 10px;
+  }
+  .textConten-n5 {
+    padding: 10px;
+    height: 100px;
+    width: 400px;
+    overflow: auto;
+  }
+  .recomen-n5 {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: stretch;
+    align-items: stretch;
+    row-gap: 2px;
+    column-gap: 2px;
+    margin-block: 10px;
+    max-width: 900px;
+    max-height: 300px;
+  }
+  .re_conten-n5 {
+    display: grid;
+    grid-template-columns: 100px auto;
+    column-gap: 10px;
+    justify-items: stretch;
+    align-items: center;
+    box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1);
+    padding: 5px;
+    box-sizing: content-box;
+  }
 }
-.btn_cart {
-  padding: 5px;
-  color: white;
-  text-decoration: none;
-  background-color: black;
-  border-radius: 5px;
-}
-.btn_cart:hover {
-  padding: 8px;
-  color: white;
-  text-decoration: none;
-  background-color: rgb(4, 60, 203);
-  border-radius: 5px;
-  box-shadow: 1px 1px 5px 2px rgba(0, 0, 0, 0.2);
-}
-.LPage {
-  display: grid;
-  grid-template-columns: 400px auto;
-  column-gap: 10px;
-  justify-items: stretch;
-  align-items: center;
-  margin-inline: 170px;
-  margin-block: 5px;
-  width: 1000px;
-  border-bottom: 2px solid;
-}
-.RPage {
-  display: grid;
-  grid-template-columns: auto 400px;
-  column-gap: 1px;
-  justify-items: stretch;
-  align-items: center;
-  margin-inline: 170px;
-  margin-block: 5px;
-  width: 1000px;
-  border-bottom: 2px solid;
-}
-.imgBow {
-  max-width: 880px;
-  margin: 10px;
-}
-.container_text {
-  padding: 5px;
-}
-.textConten {
-  padding: 20px;
-  width: 500px;
-}
-.recomen {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  justify-items: stretch;
-  align-items: stretch;
-  row-gap: 2px;
-  column-gap: 2px;
-  max-width: 1000px;
-  margin-inline: 200px;
-  margin-block: 10px;
-}
-.re_conten {
-  display: grid;
-  grid-template-columns: 200px auto;
-  column-gap: 10px;
-  justify-items: stretch;
-  align-items: center;
-  box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1);
-  padding: 5px;
-  border-radius: 5px;
+@media (max-width: 767px) {
+  .container-n5 {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 10px;
+    justify-items: center;
+    align-items: stretch;
+  }
+  .imgTitle {
+    max-width: 100%;
+    background-position: left bottom;
+    background-size: cover;
+    padding-top: 200px;
+    padding-bottom: 100px;
+    background-image: url(https://i.pinimg.com/564x/a4/11/bf/a411bfbf71e49fc1a525fccf0bbc0d40.jpg);
+  }
+  .btn_cart-n5 {
+    padding: 5px;
+    color: white;
+    text-decoration: none;
+    background-color: black;
+    border-radius: 5px;
+  }
+  .btn_cart-n5:hover {
+    padding: 8px;
+    color: white;
+    text-decoration: none;
+    background-color: rgb(4, 60, 203);
+    border-radius: 5px;
+  }
+  .LPage-n5 {
+    display: grid;
+    grid-template-columns: 1fr;
+    justify-items: stretch;
+    border-bottom: 2px solid;
+    margin-block: 5px;
+    max-width: 300px;
+  }
+  .RPage-n5 {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap-reverse;
+    justify-content: center;
+    align-items: stretch;
+    justify-items: stretch;
+    max-width: 300px;
+    margin-block: 5px;
+    border-bottom: 2px solid;
+  }
+  .imgBow {
+    max-width: 880px;
+    margin: 10px;
+  }
+  .textConten-n5 {
+    padding: 10px;
+    height: 150px;
+    width: 300px;
+    overflow: auto;
+  }
+  .recomen-n5 {
+    display: grid;
+    grid-template-columns: 1fr;
+    justify-items: stretch;
+    align-items: stretch;
+    row-gap: 2px;
+    column-gap: 2px;
+    margin-block: 10px;
+    max-width: 600px;
+  }
+  .re_conten-n5 {
+    display: grid;
+    grid-template-columns: 100px auto;
+    column-gap: 10px;
+    justify-items: stretch;
+    align-items: center;
+    box-sizing: content-box;
+    box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1);
+    padding: 5px;
+  }
 }
 </style>
